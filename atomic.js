f921daf9f4d8ac83623ee2906d296f48132a44ea
Joshua Bloom <bloomblockchain@gmail.com>
2022-11-14 09:59:47 -0500

    Finished create NFT function, array ID, signed transaction array and the final async function where the logic is called.

--- atomic.js
+++ atomic.js
@@ -21,51 +21,96 @@ const submitToNetwork = async (signedTxn) => {
   return confirmedTxn;
 };
 
-const submitAtomicTransfer = () => {
+// 0. creator creates the NFT
 
-  // 1. Buyer account pays 1 Algo to the creator.
-
-  const tx1 = () => {
-
-  };
-
-  // 2. Buyer opts into the asset. 
-  const tx2 = () => {
-
-  };
-
-  // 3. Creator sends the NFT to the buyer.
-  const tx3 = () => {
-
-  };
-
-  // 4. Creator sends 10% of the payment to the artist's account.
-  const tx4 = () => {
-
-  };
-
-
-  txnArrayId = [tx1, tx2, tx3, tx4];
-
-  // here the array has an Id attached to it
-  
-  
-  // here we sign the transactions
-  txn1Signed = tx1.signedTxn(creator.sk);
-  txn2Signed = txn2.signedTxn(creator.sk);
-  txn3Signed = txn3.signedTxn(creator.sk);
-  txn4Signed = txn4.signedTxn(creator.sk);
-  
-  // now we create an array of signed transactions
-  txnSignedArray = [];
-  
-  const submitAtomic = submitToNetwork(txnSignedArray);
-
-  return submitAtomic;
-
+const createNFT = async () => {
+  const sugParams = await algodClient.getTransactionParams().do();
+  const txn = algosdk.makeAssetCreateTxnWithSuggestedParams(
+    creator.addr,
+    undefined,
+    1,
+    0,
+    undefined,
+    creator.addr,
+    undefined,
+    undefined,
+    undefined,
+    "TST1",
+    "Test NFT One",
+    undefined,
+    undefined,
+    sugParams,
+    undefined,
+  );
+  const signTxn = txn.signTxn(creator.sk);
+  const confirmedTxn = await submitToNetwork(signTxn);
+  return 
 };
 
+const submitAtomicTransfer = async () => {
+  try {
+    // 1. Buyer account pays 1 Algo to the creator.
+    const tx1 = (assetId) => {
+      const txn = algosdk.makeAssetTransferTxnWithSuggestedParams(
+          receiver.addr, // receiver sends
+          creator.addr, // creator receives
+          undefined,
+          undefined,
+          1000000,
+          undefined,
+          assetId
+      );
+    };
+
+    // 2. Buyer opts into the asset. 
+    const tx2 = () => {
+
+    };
+
+    // 3. Creator sends the NFT to the buyer.
+    const tx3 = () => {
+
+    };
+
+    // 4. Creator sends 10% of the payment to the artist's account.
+    const tx4 = () => {
+
+    };
+
+    // put unsigned transactions in an array to be id'ed
+    let txnArrayId = [tx1, tx2, tx3, tx4];
+    // here the array has an Id attached to it 
+    let txnGroup = algosdk.assignGroupID(txnArrayId);
+    
+    // here we sign the transactions
+    let txn1Signed = txn1.signedTxn(creator.sk);
+    let txn2Signed = txn2.signedTxn(creator.sk);
+    let txn3Signed = txn3.signedTxn(creator.sk);
+    let txn4Signed = txn4.signedTxn(creator.sk);
+    
+    // now we create an array of signed transactions
+    let txnSignedArray = [];
+    txnSignedArray.push(txn1Signed);
+    txnSignedArray.push(txn2Signed);
+    txnSignedArray.push(txn3Signed);
+    txnSignedArray.push(txn4Signed);
+    
+    // submit atomic transaction to the network
+    await submitToNetwork(txnSignedArray);
+
+    // print account balances
+    console.log(await algodClient.accountInformation(ADDRESS_CREATOR).do());
+    console.log(await algodClient.accountInformation(ADDRESS_RECEIVER).do());
+
+  } catch (err) {
+    console.log('err', err);
+  }
+};
 
 (async () => {  
-
+    console.log("Creating NFT!");
+    createNFT();
+    console.log("Submitting atomic transfer!");
+    submitAtomicTransfer(assetId);
+    console.log("Atomic transfer complete!");
 })();
